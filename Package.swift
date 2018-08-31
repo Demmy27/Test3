8a9135ae7b2fb91f8c6ed2317fa06c6b0c25c92e
Shishov_DR <d1men27@mail.ru>
2018-08-31 17:42:15 +0400

    swiftyJson

--- Package.swift
+++ Package.swift
@@ -6,21 +6,13 @@ import PackageDescription
 let package = Package(
     name: "Test3",
     dependencies: [
-        .package(
-            url: "https://github.com/Alamofire/Alamofire.git",
-            from: "4.0.0"
-        )
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.0.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Test3",
-            dependencies: ["Test3Core"]
-        ),
-        .target(name: "Test3Core",
-        dependencies: ["Alamofire"]
-        )
-        
+        .target( name: "Test3", dependencies: ["Test3Core"]),
+        .target(name: "Test3Core", dependencies: ["Alamofire", "SwiftyJSON"])
     ]
 )
--- Sources/Test3Core/Test3.swift
+++ Sources/Test3Core/Test3.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 public final class Test3 {
     private let arguments: [String]
@@ -9,13 +10,59 @@ public final class Test3 {
     }
     
     public func run() throws {
-        guard arguments.count > 1 else {
-            print("Hello World")
-            return
-        }
+//        guard arguments.count > 1 else {
+//            print("Hello World")
+//            return
+//        }
         // The first argument is the execution path
-        let fileName = arguments[1]
+//        let fileName = arguments[1]
         
-        print(fileName)
+//        print(fileName)
+ 
+        print("Hello World")
+        
+        downloadTags(contentID: "2") { (resultsTags) in
+            guard let resultsTags = resultsTags else {
+                print("Error while downloadTags")
+                return;
+            }
+            for item in resultsTags {
+                print(item)
+            }
+        }
+//        var shouldQuit = false
+//        while !shouldQuit {
+//            let first = getInput()
+//            if first == "q" {
+//                shouldQuit = true
+//            }
+//        }
+        RunLoop.main.run()
+    }
+    
+    func getInput() -> String {
+        let keyboard = FileHandle.standardInput
+        let inputData = keyboard.availableData
+        let strData = String(data: inputData, encoding: String.Encoding.utf8)!
+        return strData.trimmingCharacters(in: CharacterSet.newlines)
+    }
+    
+    func downloadTags(contentID: String, completion: @escaping ([String]?) -> Void) {
+        Alamofire.request("https://api.github.com/users/Demmy27/repos")
+            .responseJSON { response in
+                
+                guard response.result.isSuccess,
+                    let value = response.result.value else {
+                        print("Error while fetching tags: \(String(describing: response.result.error))")
+                        completion(nil)
+                        return
+                }
+//                print(response.result.value!)
+                let tags = JSON(value).array?.map { json in
+                    json["name"].stringValue
+                }
+                
+                completion(tags)
+        }
     }
 }
